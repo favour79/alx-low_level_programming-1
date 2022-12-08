fe8d126024908c6e7c0174e08c73458ff6c6302e
Favour79 <favour.chukwu@aol.com>
2022-12-08 13:36:17 -0800

    Task 7

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 /**
  * main - print "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *              followed by a new line, to standard error.
+ *                followed by a new line, to standard error.
  *
- * Return: 1
+ * Return: Always 1.
  */
 int main(void)
 {
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
 
-	("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	 59);
 	return (1);
 }
