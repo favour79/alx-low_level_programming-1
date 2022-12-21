363c741c272ba79464c062f6f35cb1d2db1a4388
Favour79 <favour.chukwu@aol.com>
2022-12-21 11:45:12 -0800

    Task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,20 +1,18 @@
 #include "main.h"
 /**
- * string_toupper - change lowercase to uppercase
- * @s:string
- * Return:char
+ * reverse_array - reverse array
+ * @a:array
+ * @n:integer
+ * Return:void
  */
-char *string_toupper(char *s)
+void reverse_array(int *a, int n)
 {
+int i, c;
 
-	int i;
-
-i = 0;
-	while (*(s + i))
+for (i = 0; (i < (n - 1) / 2); i++)
 	{
-		if (*(s + i) >= 'a' && *(s + i) <= 'z')
-			*(s + i) -= 'a' - 'A';
-		i++;
+	c = a[i];
+	a[i] = a[n - 1 - i];
+	a[n - 1 - i] = c;
 	}
-	return (s);
 }
